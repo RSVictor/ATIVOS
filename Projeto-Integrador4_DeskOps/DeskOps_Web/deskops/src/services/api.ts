aa948593b6283e4839ed0d66fd095f5087c3cee5
RSVictor <victorribeiro304@hotmail.com>
2025-12-11 15:02:52 -0300

    Corrigido

--- Projeto-Integrador4_DeskOps/DeskOps_Web/deskops/src/services/api.ts
+++ Projeto-Integrador4_DeskOps/DeskOps_Web/deskops/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://deskops-v1-cyfhfecpc0decmd8.westus3-01.azurewebsites.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
